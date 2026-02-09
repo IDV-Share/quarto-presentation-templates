fa30b6f03c2a561d8bec7b1b4e84f856f0d22d33
mtt-idv <matteo.larcher@ivecogroup.com>
2026-02-09 16:25:43 +0100

    added more font sizes choices in custom-font-size lua filter

--- _extensions/IDV-Share/quarto-presentation-templates/custom-font-size.lua
+++ _extensions/IDV-Share/quarto-presentation-templates/custom-font-size.lua
@@ -1,4 +1,4 @@
--- reduce-font.lua
+-- custom-font-size.lua
 -- Reliable local font size reduction for PPTX using OpenXML
 
 local function is_pptx()
@@ -7,8 +7,16 @@ end
 
 -- PowerPoint font size units: 1/100 pt
 local SIZES = {
+  LARGE = 2800,       -- 28 pt
+  Large = 2400,       -- 24 pt
+  large = 2200,       -- 22 pt
+  normal = 2000,      -- 20 pt
   small = 1800,       -- 18 pt
-  ["very-small"] = 1400  -- 14 pt
+  Small = 1600,       -- 16 pt
+  SMALL = 1400,       -- 14 pt
+  tiny = 1200,        -- 12 pt
+  Tiny = 1000,        -- 10 pt
+  TINY = 800,         -- 8 pt
 }
 
 function Span(el)
